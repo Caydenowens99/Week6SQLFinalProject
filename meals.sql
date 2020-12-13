050e3ca53e6262b9c7a5ca2057259d6904e2857f
LoganHenning <lrhenning13@gmail.com>
2020-12-13 09:47:57 -0600

    initial commit

---
+++ meals.sql
@@ -0,0 +1,44 @@
+create database if not exists meals;
+use meals;
+
+drop table if exists meal_dish;
+drop table if exists dish_stats;
+drop table if exists dish;
+drop table if exists meal;
+
+create table meal(
+	id int(10) not null auto_increment,
+    meal_type varchar(20) NOT NULL,
+    primary key(id)
+    );
+    
+create table dish(
+	id int(10) not null auto_increment,
+    dish_name varchar(20) not null,
+    dish_type varchar(20) not null,
+    primary key(id)
+    );
+    
+insert into dish
+	(dish_name, dish_type)
+    values("Cereal", "MAIN"),
+		("Pizza", "MAIN"),
+        ("Lasagna", "MAIN"),
+        ("Peas", "SIDE"),
+        ("Corn", "SIDE"),
+        ("Fries", "SIDE");
+        
+create table meal_dish(
+	meal_id int(10) not null,
+    dish_idk int(20) not null,
+    foreign key(meal_id) references meal(id),
+    foreign key(dish_id) references dish(id)
+    );
+    
+create table dish_stats(
+	id int(10) not null auto_increment,
+    dish_id int(10) not null,
+    count int(10) not null,
+    primary key(id),
+    foreign key(dish_id) references dish(id) on delete cascade
+    );
